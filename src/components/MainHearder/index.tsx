4b3a6d2d909dd681a89f579279569a7e453d0c3b
AmandaRiosAmaral <amandariosamaral@gmail.com>
2022-11-16 20:58:56 +0100

    add component MainHeader

---
+++ src/components/MainHearder/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from './styles';
+
+const MainHeader: React.FC = () => {
+    return (
+        <Container />
+    );
+};
+
+export default MainHeader;
