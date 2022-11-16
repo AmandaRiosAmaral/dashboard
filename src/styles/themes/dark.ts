2b682f1ab04661fac0420444bc64dd5a78c0284b
AmandaRiosAmaral <amandariosamaral@gmail.com>
2022-11-16 20:56:42 +0100

    add themes

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,13 @@
+export default {
+    title: 'dark',
+
+    color: {
+        primary: '#3A2F66',
+        secondary: '#2A2347',
+        tertiary: '#271865',
+
+        success: '#4E41F0',
+        info: '#F7931B',
+        warning: '#E44C4E',
+    },
+};
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,13 @@
+export default {
+    title: 'light',
+
+    color: {
+        primary: '#F2EFE7',
+        secondary: '#EAE6DA',
+        tertiary: '#E7E1D0',
+
+        success: '#252A48',
+        info: '#F7931B',
+        warning: '#E44C4E',
+    },
+};
